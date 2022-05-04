3da3a133584f338dbae32635dbfd8f4329ec4618
CaptainLev5 <99326745+CaptainLev5@users.noreply.github.com>
2022-05-04 04:12:29 +0300

    Coursework

---
+++ coursework/coursework/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace coursework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.MaximumSize = new System.Drawing.Size(880, 586);
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = true;
+            pictureBox3.Visible = true;
+            pictureBox4.Visible = true;
+            pictureBox5.Visible = true;
+            pictureBox6.Visible = false;
+            pictureBox7.Visible = false;
+            pictureBox1.Visible = true;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+            pictureBox4.Visible = false;
+            pictureBox5.Visible = false;
+            pictureBox6.Visible = true;
+            pictureBox7.Visible = true;
+            pictureBox1.Visible = false;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e) // просмотреть
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+        }
+        private void pictureBox4_Click(object sender, EventArgs e) // редактировать
+        {
+            Form3 f3 = new Form3();
+            f3.Show();
+        }
+        private void pictureBox5_Click(object sender, EventArgs e) // услуги
+        {
+            Form4 f4 = new Form4();
+            f4.Show();
+        }
+    }
+}
---
+++ coursework/coursework/Form2.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace coursework
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(86, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Комплектующие";
+            this.button1.UseVisualStyleBackColor = true;
+            
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(246, 328);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 59);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Поставщики";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(422, 328);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 59);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Заказчики";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(595, 328);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(129, 59);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Сервисы";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 476);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form2";
+            this.Text = "Просмотр";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
